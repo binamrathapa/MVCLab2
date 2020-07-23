03fffa108cd67f19be7e28722e17695cb1640996
binamrathapa <binamrathapa@users.noreply.github.com>
2020-07-23 10:00:22 +0545

    Add files via upload
    lab2

---
+++ WebApplication_MVC/Models/MVCClassContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_MVC.Models
+{
+    public class MVCClassContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ WebApplication_MVC/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_MVC.Models
+{
+    public class Student
+    {
+        public int Id
+        {
+            get;set;
+        }
+        [Required(ErrorMessage ="Name is required")]
+        public string Name
+        { get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email
+        { get; set; }
+        public string Address
+        { get; set; }
+    }
+}
